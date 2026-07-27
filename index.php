4dafd848e16a9a22ca8b59f0ee910ac7c07624b3
Akhil gusain <akhilgusain2@gmail.com>
2026-07-28 01:56:23 +0530

    dd

--- index.php
+++ index.php
@@ -195,7 +195,7 @@ Email: info@everythingeasy.in
           background-size: cover !important;
         }
         .main-slider-one .hero-main-image {
-          background-size: 104% auto !important;
+          background-size: 101% auto !important;
         }
         .main-slider.main-slider-one .swiper-container-initialized .swiper-slide:not(.swiper-slide-active) {
           opacity: 0 !important;
@@ -314,7 +314,7 @@ Email: info@everythingeasy.in
             background-size: contain !important;
           }
           .main-slider.main-slider-one .hero-main-image {
-            background-size: 114% auto !important;
+            background-size: 108% auto !important;
           }
           .main-slider.main-slider-one .container {
             height: 100%;
